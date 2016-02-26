4609d5b0182055a1907a7f3bfcca016d9ec866e0
Rene Hollander <rene.hollander@hotmail.de>
2016-02-26 10:43:37 +0100

    started work on chat

---
+++ webapplication/src/main/java/at/renehollander/mobileapp/chat/ChatHandler.java
@@ -0,0 +1,13 @@
+package at.renehollander.mobileapp.chat;
+
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+public class ChatHandler {
+
+//    Queue<DeferredResult<List<Map<String, Object>>>> connectionQueue
+
+}
